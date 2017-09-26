708d0768a07aeeb913bf0c3f9b0863c84de22fdd
KaiserChan <chankaiserkk@gmail.com>
2017-09-25 22:04:59 -0400

    updated dictionary.rb .. completed all methods

--- dictionary.rb
+++ dictionary.rb
@@ -1,24 +1,28 @@
 class Dictionary
 
-  def initialize(hash={})
-    @d = hash
+  def initialize
+    @d = {}
   end
 
   def entries
     @d
   end
 
-  def include?(key=nil)
-    @d.has_key?(key)
+  def include?(word)
+    @d.has_key?(word)
   end
 
-  def add(hash)
-    @d[hash.keys.join] = hash.values.join
+  def add(entry)
+    if entry.is_a?(Hash)
+      @d.merge!(entry)
+    else
+      @d[entry] = nil;
+    end
   end
 
-  def find(string)
-    if @d.has_key?(string)
-      return @d.first
+  def find(word)
+    @d.select do |k, v|
+      k.start_with?(word)
     end
   end
 
